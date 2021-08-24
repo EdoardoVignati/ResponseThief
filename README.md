1c5b5395116fb395c82761e3d183dc3f1eb5bb86
Edoardo Vignati <edoardo.vignati@gmail.com>
2021-08-24 21:02:16 +0200

    refactoring and fixes

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ResponseThief
-ResponseThief visits URLs from a list and returns the http response codes. 
+
+ResponseThief visits URLs from a list and returns the http response codes.
 
      ___                            _____ _    _      __ 
     | _ \___ ____ __ ___ _ _  _____|_   _| |_ (_)___ / _|
@@ -8,55 +9,66 @@ ResponseThief visits URLs from a list and returns the http response codes.
                |_|                                       	                               
                                        by @EdoardoVignati 
 
-## Requirements for Python3
-- from termcolor import colored, cprint
-- import threading
-- import requests
-- import argparse
-- import signal
-- import time
-- import sys
-- import os
-- import re
+## Installation
+
+```$ pip install -r requirements.txt```
 
 ## Usage
-Clone the repo and run 
 
-```
-$ ./response-thief.py inputfile.txt outputfile.txt
+Use Python3.
+
+Clone the repo and run:
+
+```bash
+$ ./response-thief.py url_sample.txt result.txt
 ```
 
 where ```inputfile.txt``` is a list of URLs without the protocol and ```outputfile.txt``` is a path to an empty file.
 
 Example of input file:
+
 ```
 www.github.com
 github.com
 foo.bar.gitlab.com
 ```
 
+## Filter
+
+You can filter a specific http status code by adding:
+
+```
+--filter 404
+```
+
+The output file will contain only the filtered url.
 
 ## Output
+
 The output is built in this format:
+
 ```
 [200] www.github.com
 [301] foo.gitlab.com
-...
+[...] ...
 ```
-Verbosity (--stdout) has 3 different levels:
-- enabled (print everything)
-- less (print important information)
-- disable (do not print anything)
+
+Verbosity (`--stdout`) has 3 different levels:
+
+- `enabled` (print everything)
+- `less` (print important information)
+- `disable` (do not print anything)
 
 ## Print on stdout
+
 - Red URL: error retrieving URL
-- White URL: ok, a response code has been registered 
+- White URL: ok, a response code has been registered
 
 ## Errors
+
 - [Timeout error] : cannot retrieve the webpage within 5 seconds
 - [Connection error] : cannot open or find the domain
 - [Unknown error] : don't know what's happening :)
 
-
 ![Demo](https://raw.githubusercontent.com/EdoardoVignati/ResponseThief/master/responsethief.png)
- 
+
--- response-thief.py
+++ response-thief.py
@@ -9,269 +9,271 @@ import sys
 import os
 import re
 
-
-
-logo=""" 
+logo = """ 
   ___                            _____ _    _      __ 
  | _ \___ ____ __ ___ _ _  _____|_   _| |_ (_)___ / _|
  |   / -_(_-| '_ / _ | ' \(_-/ -_)| | | ' \| / -_|  _|
  |_|_\___/__| .__\___|_||_/__\___||_| |_||_|_\___|_|  
             |_|
                                    by @EdoardoVignati                                  
-	"""
+"""
+
 # Global variables
 stdout = "enabled"
 errors = True
-responses=[]
-done=0
-total=0;
-summary={}
-maxUrl=0
+responses = []
+done = 0
+total = 0
+summary = {}
+maxUrl = 0
 lock = threading.Lock()
 print_lock = threading.Lock()
-filter_status=None
+filter_status = None
 
 
-def isError(s):
-	error_status=["Connection_error", "Timeout_error", "Unknown_error"]
-	return s in error_status
+def is_error(s):
+    error_status = ["Connection_error", "Timeout_error", "Unknown_error"]
+    return s in error_status
 
 
 def signal_handler(sig, frame):
-	sys.stdout.flush()
-	print('\n\nAborted!')
-	os._exit(130)
+    sys.stdout.flush()
+    print('\n\nAborted!')
+    exit(130)
 
 
-def checkUrl(url):
-	try:
-		checked = re.findall('(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', url)
-		#checked = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', url)
-	except:
-		return False
-	
-	return len(checked)==1
+def check_url(url):
+    try:
+        checked = re.findall("(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", url)
+        # checked = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', url)
+    except:
+        return False
+
+    return len(checked) == 1
+
 
 def visited(url, status):
-	global lock	
-	lock.acquire()
+    global lock
+    lock.acquire()
 
-	global summary
-	global responses
-	if (url,status.status_code) not in responses: 
-		responses.append((url,status.status_code))
-		if status.status_code in summary.keys():
-			summary[status.status_code]=summary[status.status_code]+1
-		else:
-			summary[status.status_code]=1
-	lock.release()
-
-
-def counted(url, status):
-	if(stdout=="disabled"):
-		return
-
-	global lock
-	lock.acquire()
-
-	global done
-	global maxUrl
-	global total
-	global errors
-	global filter_status
-
-	done+=1
-	padding=(" " *(maxUrl-len(url)+27))
-
-	print_lock.acquire()
-	sys.stdout.write("Visited... " + str(("{:.2f}").format(round((done/total)*100,2))) + "%" + padding + "\r")
-	sys.stdout.flush()
-	
-	if stdout=="enabled":
-
-		if not isError(str(status)):
-			code=status.status_code
-		else:
-			code=status
-
-		if(filter_status!=None and str(code)==filter_status) or  filter_status==None: 
-			if not errors:
-				if not isError(str(status)):
-					print("Visited " + colored(str(done) + "/" + str(total),"green") + " -> " + url + padding)
-			elif errors:
-				if isError(str(status)):
-					print("Visited " + colored(str(done) + "/" + str(total),"green") + " -> " + colored(url,"red") + padding)
-				else:
-					print("Visited " + colored(str(done) + "/" + str(total),"green") + " -> " + url + padding)
-		print_lock.release()	
-	lock.release()
-	return done
+    global summary
+    global responses
+    if (url, status.status_code) not in responses:
+        responses.append((url, status.status_code))
+        if status.status_code in summary.keys():
+            summary[status.status_code] = summary[status.status_code] + 1
+        else:
+            summary[status.status_code] = 1
+    lock.release()
 
 
-def visitThread(url, to):
-	global lock
-	global responses
-	global stdout
-		
-	try:
+def counter(url, status):
+    if stdout == "disabled":
+        return
 
-		# Test HTTPS
-		status = requests.head("https://"+url.strip(), timeout=to, allow_redirects=True)
-		# Test HTTP
-		status = requests.get("http://"+url.strip(), timeout=to, allow_redirects=True)
+    global lock
+    global done
+    global maxUrl
+    global total
+    global errors
+    global filter_status
 
-		# Url done
-		visited(url, status)
-		counted(url, status)
+    lock.acquire()
+    done += 1
+    padding = (" " * (maxUrl - len(url) + 27))
 
-	except requests.ConnectionError:
-		lock.acquire()
-		responses.append((url,"Connection_error"))
-		lock.release()
-		counted(url, "Connection_error")
-	except requests.exceptions.Timeout:
-		lock.acquire()
-		responses.append((url,"Timeout_error"))
-		lock.release()
-		counted(url, "Timeout_error")
-	except:
-		lock.acquire()
-		responses.append((url,"Unknown_error"))
-		lock.release()
-		counted(url, "Unknown_error")
+    print_lock.acquire()
 
+    if stdout == "enabled" or stdout == "minimal":
+        if not is_error(str(status)):
+            code = status.status_code
+        else:
+            code = status
+        if (filter_status is not None and str(code) == filter_status) or filter_status is None:
+            green = "Visited {} -> [{}] {}{}".format(colored(str(done) + "/" + str(total), "green"),
+                                                     code, url, padding)
+            red = "Visited {} -> [{}] {}{}".format(colored(str(done) + "/" + str(total), "green"),
+                                                  code, colored(url, "red"), padding)
+            if not errors:
+                if not is_error(str(status)):
+                    print(green)
+            elif errors:
+                if is_error(str(status)):
+                    print(red)
+                else:
+                    print(green)
+        if stdout == "enabled":
+            sys.stdout.write("Visited... {}%{}\r".format(str("{:.2f}".format(round((done / total) * 100, 2))), padding))
+            sys.stdout.flush()
 
-def readFile(inputfile):
-	global urlList
-	global total
-	global maxUrl
+    print_lock.release()
+    lock.release()
 
-	urlList = []
-	with open(inputfile) as fp:
-		line = fp.readline().strip()
-		while line:
-			if (line!="") and (line!="\n") and checkUrl(line) and (line not in urlList):
-				urlList.append(line)
-				total+=1
-				if(len(line.strip())>maxUrl):
-					maxUrl=len(line.strip())
-			line = fp.readline().strip()
-	if stdout!="disabled":
-		print("Total URLs: " + str(total) + "\n")
-	maxUrl+=len("Launching ")+(len(str(total))*2)+1
-	return urlList
+    return done
 
 
-def writeOutput(outputfile):
+def visit_thread(raw_url, to):
+    global lock
+    global responses
+    global stdout
 
-	global responses
-	global stdout
-	global errors
-	global filter_status
+    url = raw_url.replace("http://", "").replace("https://", "")
 
-	try:
-		os.remove(outputfile)
-	except:
-		pass
-	out = open(outputfile, 'a')
-	length = float(len(responses))
-	i=0.0
+    try:
+        # Test HTTPS
+        status = requests.head("https://" + url.strip(), timeout=to, allow_redirects=True)
+        # Test HTTP
+        status = requests.get("http://" + url.strip(), timeout=to, allow_redirects=True)
 
-	for u in responses:
-		if(filter_status!=None and str(u[1])==filter_status) or  filter_status==None: 
-			if not errors:		
-				if not isError(u[1]):				
-					out.write("[" + str(u[1]) + "] " + u[0] + "\n")
-			else:
-				out.write("[" + str(u[1]) + "] " + u[0] + "\n")
-		
-		i+=1.0
-		if(stdout=="enabled"):
-			padding=(" " *(maxUrl+27))
-			sys.stdout.write("Writing... " + str(format(round((i/length)*100,2))) + "%" + padding + "\r")
-			sys.stdout.flush()
-	out.close()
+        # Url done
+        visited(url, status)
+        counter(url, status)
+
+    except requests.ConnectionError:
+        lock.acquire()
+        responses.append((url, "Connection_error"))
+        lock.release()
+        counter(url, "Connection_error")
+    except requests.exceptions.Timeout:
+        lock.acquire()
+        responses.append((url, "Timeout_error"))
+        lock.release()
+        counter(url, "Timeout_error")
+    except:
+        lock.acquire()
+        responses.append((url, "Unknown_error"))
+        lock.release()
+        counter(url, "Unknown_error")
 
 
-def printSummary():
-	if(stdout!="disabled"):
-		print("\n")
-		for k in summary.keys():
-			print("Response [" + str(k) + "] -> " + str(summary[k]) + " times")
-		print("")
-		print("Output saved in '" + os.path.abspath(outputfile) + "'")
+def read_file(file_input):
+    global urlList
+    global total
+    global maxUrl
 
+    urlList = []
+    with open(file_input) as fp:
+        l = fp.readline().strip()
+        while l:
+            if (l != "") and (l != "\n") and check_url(l) and (l not in urlList):
+                urlList.append(l)
+                total += 1
+                if len(l.strip()) > maxUrl:
+                    maxUrl = len(l.strip())
+            l = fp.readline().strip()
+    if stdout == "enabled":
+        print("Total URLs: " + str(total) + "\n")
+    maxUrl += len("Launching ") + (len(str(total)) * 2) + 1
+    return urlList
+
+
+def write_output(file_output):
+    global responses
+    global stdout
+    global errors
+    global filter_status
+
+    try:
+        os.remove(file_output)
+    except:
+        pass
+    out = open(file_output, "a")
+    length = float(len(responses))
+    write_count = 0.0
+
+    for u in responses:
+        if (filter_status is not None and str(u[1]) == filter_status) or filter_status is None:
+            if not errors:
+                if not is_error(u[1]):
+                    out.write("[" + str(u[1]) + "] " + u[0] + "\n")
+            else:
+                out.write("[" + str(u[1]) + "] " + u[0] + "\n")
+
+        write_count += 1.0
+        if stdout == "enabled":
+            padding = (" " * (maxUrl + 27))
+            sys.stdout.write("Writing... {}%{}\r".format(round((write_count / length) * 100, 2), padding))
+            sys.stdout.flush()
+    out.close()
+
+
+def print_summary():
+    if stdout != "disabled":
+        print("\n")
+        for k in summary.keys():
+            print("Response [{}] -> {} times".format(str(k), str(summary[k])))
+        if stdout == "enabled":
+            print("")
+            print("Output saved in '" + os.path.abspath(file_output) + "'")
 
 
 if __name__ == "__main__":
 
-	inputfile=None
-	outputfile=None
+    desc = "Given a list of URLs, tests the HTTP response codes and write them into a file. "
+    desc += "URLs must not begin with the protocol ('http://' or 'https://')\n"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('input', metavar='file_input', help='path of input file. A list of URLs, each for row.')
+    parser.add_argument('output', metavar='file_output',
+                        help='path of output file. An empty file to append output. Should be different from file_input')
+    parser.add_argument('--stdout', metavar='[enabled, minimal, disabled]', help='edit verbosity of stdout print.')
+    parser.add_argument('--errors', metavar='[yes, no]', help='save or not errors')
+    parser.add_argument('--filter', metavar='[200, 301, ..]', help='a status code to filter')
+    args = parser.parse_args()
 
-	desc = "Given a list of URLs, tests the HTTP response codes and write them into a file. "
-	desc+="URLs must not begin with the protocol ('http://' or 'https://')\n"
-	parser = argparse.ArgumentParser(description=desc)
-	parser.add_argument('input', metavar='inputfile', help='path of input file. A list of URLs, each for row.')
-	parser.add_argument('output', metavar='outputfile', help='path of output file. An empty file to append output.')
-	parser.add_argument('--stdout', metavar='[enabled, less, disabled]', help='edit verbosity of stdout print.')
-	parser.add_argument('--errors', metavar='[yes, no]', help='save or not errors')
-	parser.add_argument('--filter', metavar='[200, 301, ..]', help='a status code to filter')
-	args = parser.parse_args()
+    file_input = args.input
+    file_output = args.output
+    filter_status = args.filter
 
+    if args.stdout is not None:
+        stdout = args.stdout
 
-	inputfile = args.input
-	outputfile = args.output
-	filter_status = args.filter
+    if args.errors is not None:
+        if args.errors == "no":
+            errors = False
 
-	if args.stdout!=None:
-		stdout=args.stdout
+    if stdout == "enabled":
+        cprint(logo, "red")
 
-	if args.errors!=None:
-		if args.errors=="no":	
-			errors=False	
+    if file_input is None or file_output is None:
+        print(parser.print_help())
+        exit(1)
 
-	if(stdout != "disabled"):
-		cprint(logo,"red")
+    if file_input == file_output:
+        print(parser.print_help())
+        exit(1)
 
-	if inputfile==None or outputfile==None:
-		print(parser.print_help())
-		exit(1)
+    try:
+        f = open(file_input)
+        f.close()
+    except IOError:
+        print("File not found: " + file_input)
+        exit(1)
 
-	try:
-		f = open(inputfile)
-	except IOError:
-		print("File not found: " + inputfile)
-		exit(1)
-	finally:
-		try:
-			f.close()
-		except Exception:
-			pass
+    signal.signal(signal.SIGINT, signal_handler)
 
-	signal.signal(signal.SIGINT, signal_handler)
+    urlList = read_file(file_input)
 
-	urlList = readFile(inputfile)
+    # Launch threads
+    launched_threads = []
+    visit_timeout = 5
+    sleep_time = 0
+    i = 0
+    for line in urlList:
+        i += 1
+        if line.strip() != "":
+            if stdout == "enabled":
+                sys.stdout.write(
+                    "Launching " + colored(str(i) + "/" + str(total), "blue") + " -> " + line.strip() + "\r")
+                sys.stdout.flush()
+            t = threading.Thread(target=visit_thread, args=(line.strip(), visit_timeout))
+            launched_threads.append(t)
+            time.sleep(sleep_time)
+            t.start()
 
-	# Launch threads
-	launched_threads=[]
-	visit_timeout=5
-	sleeptime=0
-	i=0
-	for line in urlList:
-		i+=1
-		if line.strip()!="":
-			if(stdout=="enabled"):
-				sys.stdout.write("Launching " + colored(str(i) + "/"+ str(total), "blue") +" -> " + line.strip() + "\r")
-				sys.stdout.flush()
-			t = threading.Thread(target=visitThread, args=(line.strip(), visit_timeout))
-			launched_threads.append(t)
-			time.sleep(sleeptime)
-			t.start()
+    # Wait for termination
+    for t in launched_threads:
+        t.join()
 
-	# Wait for termination
-	for t in launched_threads:
-		t.join()
+    write_output(file_output)
 
-	writeOutput(outputfile)
-
-	printSummary()
+    print_summary()
